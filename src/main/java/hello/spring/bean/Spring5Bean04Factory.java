f372277217ef170e7c383e160915d3ffe0e46908
NaseulChoi <just.come.as.you.are.babe@gmail.com>
2023-06-23 13:58:46 +0900

    인삿말 객체를 생성하는 팩토리 클래스

---
+++ src/main/java/hello/spring/bean/Spring5Bean04Factory.java
@@ -0,0 +1,21 @@
+package hello.spring.bean;
+
+public class Spring5Bean04Factory {
+
+    //인사말을 출력하는 객체를 생성하는 create 메서드
+    //최상위를 불러오면 하위를 사용 가능
+    public static Spring5Bean03 create(String bean){
+        Spring5Bean03 obj=null;
+
+        if(bean.equals("kor"))
+            obj = new Spring5Bean03Kor();
+        else if(bean.equals("eng"))
+            obj = new Spring5Bean03Eng();
+        else if(bean.equals("Jpn"));
+            obj = new Spring5Bean03Jpn();
+
+
+        return obj;
+
+    }
+}
