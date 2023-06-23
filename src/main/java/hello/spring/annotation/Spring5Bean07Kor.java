c3045627174b4a61188472b7852f2a6177b96d1a
NaseulChoi <just.come.as.you.are.babe@gmail.com>
2023-06-23 15:11:23 +0900

    Annotation을 이용한 인삿말 빈 생성

---
+++ src/main/java/hello/spring/annotation/Spring5Bean07Kor.java
@@ -0,0 +1,14 @@
+package hello.spring.annotation;
+
+import hello.spring.bean.Spring5Bean03;
+import org.springframework.stereotype.Component;
+
+
+@Component("kor")
+public class Spring5Bean07Kor implements Spring5Bean03 {
+    public void sayHello(String msg) {
+        System.out.println("안녕하세요," + msg);
+    }
+}
+//인터페이스 정의
+//메서드 정의
