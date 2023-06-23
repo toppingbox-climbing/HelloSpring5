ab49ec484f222f38ea92a4a15c801755ae12b455
NaseulChoi <just.come.as.you.are.babe@gmail.com>
2023-06-23 14:46:04 +0900

    스프링 IoC 컨테이너를 이용한 인삿말 빈 생성

--- src/main/java/hello/spring/bean/Spring5Bean03Jpn.java
+++ src/main/java/hello/spring/bean/Spring5Bean03Jpn.java
@@ -2,8 +2,6 @@ package hello.spring.bean;
 
 public class Spring5Bean03Jpn implements Spring5Bean03 {
 
-
-
     public void sayHello(String msg) {
         System.out.println("こんにちは," + msg);
     }
